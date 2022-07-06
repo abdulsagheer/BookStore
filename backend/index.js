4706f447cb3cc07759f505fea6ecfe0e9110a41d
abdulsagheer <abdulsagheeras29@gmail.com>
2022-07-06 10:40:09 +0530

    connected frontend to backend

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/book-route");
+const cors = require("cors");
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use("/books", router); // localhost:5000/books
+
+mongoose
+  .connect(
+    "mongodb+srv://test:test@cluster0.l6ahl.mongodb.net/test?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected To Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => console.log(err));
